19381ff931d81e2d8c755ce9c3dc0f534c44c00c
Ryan Campbell <campbellryan954@gmail.com>
2023-02-07 19:04:17 -0500

    Commit 0-6

--- 06-infinite.sh
+++ 06-infinite.sh
@@ -3,10 +3,13 @@
 # the if statement had, if-then-fi
 # while loop has, while-do-done
 # true and false are also Unix commands
-while [ true ]; do
-  echo "infinite number of beer on the wall"
-done
+
 
 # exercise: write a script that continues to look up
 # the ip address of a given hostname (using nslookup) until
 # the user decides to stop
+echo "What website would you like to look up"
+read name
+while [ true ]; do
+    nslookup $name
+done
